1ecc41bc65c5518497d07f0e31785cc30a64df90
topvirtualbox <topvirtualbox@gmail.com>
2021-05-11 20:36:42 -0300

    Add playerChoice()

--- script.js
+++ script.js
@@ -8,3 +8,14 @@ function computerChoice() {
         return "Scissors";
     }
 }
+
+function playerChoice() {
+    let choice = prompt("Rock, Paper or Scissors?").toLowerCase();
+    choice = choice[0].toUpperCase() + choice.slice(1);
+    while (choice !== "Rock" && choice !== "Paper" && choice !== "Scissors") {
+        console.log("INVALID ANSWER");
+        choice = prompt("Rock, Paper or Scissors?").toLowerCase();
+        choice = choice[0].toUpperCase() + choice.slice(1);
+    }
+    return choice;
+}
