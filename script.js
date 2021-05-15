e661ca72e105eee92bc96decc5d3af651bddee92
topvirtualbox <topvirtualbox@gmail.com>
2021-05-14 21:25:12 -0300

    Remove playerChoice() and playGame()

--- script.js
+++ script.js
@@ -28,17 +28,6 @@ function computerChoice() {
     }
 }
 
-function playerChoice() {
-    let choice = prompt("Rock, Paper or Scissors?").toLowerCase();
-    choice = choice[0].toUpperCase() + choice.slice(1);
-    while (choice !== "Rock" && choice !== "Paper" && choice !== "Scissors") {
-        console.log("INVALID ANSWER");
-        choice = prompt("Rock, Paper or Scissors?").toLowerCase();
-        choice = choice[0].toUpperCase() + choice.slice(1);
-    }
-    return choice;
-}
-
 function playRound(player, computer) {
     if (player === "Rock" && computer === "Scissors" || player === "Paper" && computer === "Rock" || player === "Scissors" && computer === "Paper") {
         return "You Win! " + player + " beats " + computer;
@@ -48,30 +37,3 @@ function playRound(player, computer) {
         return "It's a Tie!";
     }
 }
-
-function playGame() {
-    let numberRound = 1;
-    let playerScore = 0;
-    let computerScore = 0;
-    while (numberRound <= 5) {
-        console.log("ROUND " + numberRound);
-        let result = playRound(playerChoice(), computerChoice());
-        if (result[4] === "W") {
-            playerScore++;
-        } else if (result[4] === "L") {
-            computerScore++;
-        }
-        console.log(result);
-        console.log("PLAYER " + playerScore + " - " + computerScore + " COMPUTER");
-        numberRound++;
-    }
-    if (playerScore > computerScore) {
-        console.log("THE PLAYER WINS!!!");
-    } else if (computerScore > playerScore) {
-        console.log("THE COMPUTER WINS!!!");
-    } else {
-        console.log("IT'S A TIE!!!");
-    }
-}
-
-playGame();
